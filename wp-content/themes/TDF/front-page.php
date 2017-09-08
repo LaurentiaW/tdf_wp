9a3cb373691d89d2bc4c3450c124d6ab68757484
laurentiaw <laurentiawehmeyer@gmail.com>
2017-09-08 11:35:18 +0200

    remove additional unused themes, add ninja form, add pages for menu

--- wp-content/themes/TDF/front-page.php
+++ wp-content/themes/TDF/front-page.php
@@ -24,33 +24,33 @@
             <section class="capabilities group">
                 <div class="skill group">
                         <figure class=cap-img>
-                           <a href="#">
-                                <img src="img/development.jpg" alt="Web development is one of The Digital Factory's core capabilities">
+                           <a href="<?php echo home_url(); ?>/about">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/development.jpg" alt="Web development is one of The Digital Factory's core capabilities">
                             </a>
                         </figure>
                         
-                        <a href="#"><h3 id=development>Development</h3></a>
+                        <a href="<?php echo home_url(); ?>/about"><h3 id=development>Development</h3></a>
                     </div>
 
                     <div  class="skill group">
                         <figure class=cap-img>
-                           <a href="#">
-                                <img src="img/design.jpg" alt="Visual Design is one of The Digital Factory's core capabilities">
+                           <a href="<?php echo home_url(); ?>/about">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/design.jpg" alt="Visual Design is one of The Digital Factory's core capabilities">
                             </a>
                         </figure>
                         
-                        <a href=""><h3 id=design>Design</h3></a>
+                        <a href="<?php echo home_url(); ?>/about"><h3 id=design>Design</h3></a>
                     </div>
 
                     <div class="skill group">
                        
                          <figure class=cap-img>
-                           <a href="#">
-                                <img src="img/cms.jpg" alt="Content Management is one of The Digital Factory's core capabilities">
+                           <a href="<?php echo home_url(); ?>/about">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/cms.jpg" alt="Content Management is one of The Digital Factory's core capabilities">
                             </a>
                         </figure>
                         
-                        <a href=""><h3 id=marketing>WordPress</h3></a>
+                        <a href="<?php echo home_url(); ?>/about"><h3 id=marketing>WordPress</h3></a>
                     </div>
                 </section> <!--end capabiliites section-->
         </div> <!--close wrapper for blue-grey background -->
@@ -58,44 +58,44 @@
 
             <section class="credentials">
                 <div class="projects">
-                    <h6>Want a <span> peak</span> of our capabilities - have a look at some of our <a href="work/index.html">past projects</a> </h6>
+                    <h6>Want a <span> peak</span> of our capabilities - have a look at some of our <a href="<?php echo home_url(); ?>/work">past projects</a> </h6>
 
                     <div class="featured-projects">
 
                         <div class="group">
                             <div class="portfolio">
                                 <figure class="portfolio-img">
-                                     <img src="img/img-tdf.png" alt="">
+                                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/img-tdf.png" alt="">
                                 </figure>
                             </div> <!--1st project -->
 
                             <div class="portfolio">
                                 <figure class="portfolio-img">
-                                     <img src="img/img-tdf.png" alt="">
+                                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/img-tdf.png" alt="">
                                 </figure> 
                             </div><!--2nd project -->
 
                             <div class="portfolio">
                                 <figure class="portfolio-img">
-                                     <img src="img/img-tdf.png" alt="">
+                                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/img-tdf.png" alt="">
                                 </figure>
                             </div><!--3rd project -->
 
                             <div class="portfolio">
                                 <figure class="portfolio-img">
-                                     <img src="img/img-tdf.png" alt="">
+                                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/img-tdf.png" alt="">
                                 </figure> 
                             </div><!--4th project -->
                         </div>
 
-                        <p class="view">click <a>here</a> to view more....</p>
+                        <p class="view">click <a href="<?php echo home_url(); ?>/work">here</a> to view more....</p>
                     </div>
                 </div><!--end of projects/credentials-->
             </section>
             <div class="fp-wrap">
                 <section class="intro">
                     <p><span>The Digital Factory:</span> A creative and practical approach to meet your digital wishes. Provide digital consulting services from digital marketing to visual design requirements and the web development skills needed to deliver your website.</p>
-                    <a href="" class="btn">Let's talk!</a>
+                    <a href="<?php echo home_url(); ?>/contact" class="btn">Let's talk!</a>
                 </section>
                 <article class="highlighted-project">
                     <div class="highlighted-project-text">
@@ -106,7 +106,9 @@
                     </div>
                     <div class="highlighted-project-img">
                         <figure>
-                            <img src="img/img-tdf.png">
+                            <a href="<?php echo home_url(); ?>/work">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/img-tdf.png">
+                            </a>
                         </figure>
                     </div>
                 </article>
