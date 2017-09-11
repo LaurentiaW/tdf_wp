35e92f1a013e32e37433d235373eea588c506e31
laurentiaw <laurentiawehmeyer@gmail.com>
2017-09-11 22:10:24 +0200

    add wordpress importer to import info from local site and backup add info about packages to the wordpress site

--- wp-content/themes/TDF/page-about.php
+++ wp-content/themes/TDF/page-about.php
@@ -32,7 +32,7 @@
             </section>
                 
                 <div class="wrapper">
-                    <h2 class="about">The Digital Factory's core capabilities are:</h3>
+                    <h2 class="about">The Digital Factory's core capabilities are:</h2>
                     <section class="capabilities group">
                             <?php query_posts('posts_per_page=3&post_type=skills'); ?>
                                 <?php while ( have_posts() ) : the_post(); 
@@ -55,7 +55,56 @@
                                 <?php endwhile; ?> 
                             <?php wp_reset_query(); ?>
                     </section>
-                </div>            
+                </div>
+                <section class="packages">
+                        <h3>How The Digital Work</h3>
+                        <p>To make it easier for clients to know what to expect The Digital Factory Web design and Development Packages</p>
+                        
+                        <ol>
+                            <li class="packages-headings">Wordpress Packages
+                                <ul>
+                                    <li>Wordpress Packages - CMS (Content Management System)
+                                        <ul>
+                                            <li>Basic WordPress website with client provided design
+                                            25 hrs implement &amp; test custom WordPress theme using placeholder content</li>
+                                            <li>5 hrs to import content & test</li>
+                                            <li>10 hrs to work with client & any revisions</li>
+                                            <li>Total 40 hrs of work <strong>&euro;2000</strong> </li>
+                                        </ul>
+                                    </li>
+                                    <li>A basic WordPress without a design
+                                        <ul>
+                                            <li>30 hrs to design the site (frontpage/homepage, about, work, blog,contact)</li>
+                                            <li>10 hrs for design revisions</li>
+                                            <li>27 hrs to code custom theme</li>
+                                            <li>5 hrs to import content &amp; test</li>
+                                            <li>10 hrs to work with client &amp; more revisions</li>
+                                            <li>Total 65 hrs to work <strong>&euro;3250</strong></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>Development Packages
+                                <ul>
+                                    <li>Develop website from design composition (Homepage/Front-page, About, Work, Blog &amp; Contact page)</li>
+                                    <li>Mobile/responsive (based on design provided)</li> 
+                                    <li>Optimizing images Development 20 hrs, Production 5 hrs, Revisions 2 hrs Total 25 hrs <strong>&euro;1250</strong></li>
+                                </ul>
+                            </li>
+                            <li>Web layout design composition Package
+                                <ul>
+                                    <li>Create a website layout design composition based on storyboards, user journeys, and wireframing </li>
+                                    <li>Deliverables include homepage/front-page, about, work, blog, and contact (or comparable to client’s needs) in mobile, tablet and desktop</li>        
+                                    <li>Meetings and brainstorming 5 hrs, Design Production 5 hrs, Design 20 hrs, Design Revision 10 hrs Total hrs 40 hrs <strong>&euro;2000</strong></li>
+                                </ul>        
+                            </li>
+                        </ol>
+                    </section>
+
+
+        
+
+
             
         <?php endwhile; else: ?>
 
